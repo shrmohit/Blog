b0eec552365c62cf0dfbf95cb7f3dcacaf6fbeca
mohit sharma <mohitsharma7258@gmail.com>
2025-05-30 13:51:17 +0530

    step frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
